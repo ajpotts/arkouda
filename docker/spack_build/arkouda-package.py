cbf7be0f025e76c192c758b6c6464c44a67bcfe8
ajpotts <ajpotts@users.noreply.github.com>
2025-09-12 12:51:12 -0400

    reformat

--- docker/spack_build/arkouda-package.py
+++ docker/spack_build/arkouda-package.py
@@ -3,14 +3,14 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 
-from spack_repo.builtin.build_systems.makefile import MakefilePackage
-
 from spack.package import *
+from spack_repo.builtin.build_systems.makefile import MakefilePackage
 
 
 class Arkouda(MakefilePackage):
     """Arkouda is a NumPy-like library for distributed data with a focus on
-    large-scale data science applications."""
+    large-scale data science applications.
+    """
 
     homepage = "https://github.com/Bears-R-Us/arkouda"
 
@@ -31,24 +31,12 @@ class Arkouda(MakefilePackage):
 
     version("main", branch="main")
 
-    version(
-        "2025.08.20", sha256="3e305930905397ff3a7a28a5d8cc2c9adca4194ca7f6ee51f749f427a2dea92c"
-    )
-    version(
-        "2025.07.03", sha256="eb888fac7b0eec6b4f3bfa0bfe14e5c8f15b449286e84c45ba95c44d8cd3917a"
-    )
-    version(
-        "2025.01.13", sha256="bb53bab92fedf43a47aadd9195eeedebe5f806d85887fa508fb5c69f2a4544ea"
-    )
-    version(
-        "2024.12.06", sha256="92ca11319a9fdeeb8879afbd1e0c9c1b1d14aa2496781c1481598963d3c37b46"
-    )
-    version(
-        "2024.10.02", sha256="00671a89a08be57ff90a94052f69bfc6fe793f7b50cf9195dd7ee794d6d13f23"
-    )
-    version(
-        "2024.06.21", sha256="ab7f753befb3a0b8e27a3d28f3c83332d2c6ae49678877a7456f0fcfe42df51c"
-    )
+    version("2025.08.20", sha256="3e305930905397ff3a7a28a5d8cc2c9adca4194ca7f6ee51f749f427a2dea92c")
+    version("2025.07.03", sha256="eb888fac7b0eec6b4f3bfa0bfe14e5c8f15b449286e84c45ba95c44d8cd3917a")
+    version("2025.01.13", sha256="bb53bab92fedf43a47aadd9195eeedebe5f806d85887fa508fb5c69f2a4544ea")
+    version("2024.12.06", sha256="92ca11319a9fdeeb8879afbd1e0c9c1b1d14aa2496781c1481598963d3c37b46")
+    version("2024.10.02", sha256="00671a89a08be57ff90a94052f69bfc6fe793f7b50cf9195dd7ee794d6d13f23")
+    version("2024.06.21", sha256="ab7f753befb3a0b8e27a3d28f3c83332d2c6ae49678877a7456f0fcfe42df51c")
 
     variant(
         "distributed",
@@ -70,7 +58,7 @@ class Arkouda(MakefilePackage):
 
     depends_on("cmake@3.13.4:", type="build")
     depends_on("python@3.9:", type=("build", "link", "run", "test"))
-    #depends_on("py-tables", type=("build", "run"), when="@2025.07.03:")
+    # depends_on("py-tables", type=("build", "run"), when="@2025.07.03:")
     depends_on("libzmq@4.2.5:", type=("build", "link", "run", "test"))
     depends_on("hdf5+hl~mpi", type=("build", "link", "run", "test"))
     depends_on("libiconv", type=("build", "link", "run", "test"))
