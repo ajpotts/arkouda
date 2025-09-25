59ff6f3b6b498b3b01a9515bb0b43356d51e7651
ajpotts <ajpotts@users.noreply.github.com>
2025-09-25 17:13:02 -0400

    working

--- arkouda/array_api/statistical_functions.py
+++ arkouda/array_api/statistical_functions.py
@@ -293,10 +293,7 @@ def prod(
     if not isinstance(x_op, pdarray):
         raise TypeError(f"Expected pdarray, got {type(x_op)}")
 
-    if axis is not None:
-        raise NotImplementedError("axis argument is not supported by arkouda.prod")
-
-    return Array._new(ak_prod(x_op))
+    return Array._new(ak_prod(x_op, axis=axis, keepdims=keepdims))
 
 
 # Not working with XArray yet, pending a fix for:
@@ -447,7 +444,7 @@ def sum(
     if not isinstance(x_op, pdarray):
         raise TypeError(f"Expected pdarray, got {type(x_op)}")
 
-    return Array._new(ak_sum(x_op))
+    return Array._new(ak_sum(x_op, axis=axis, keepdims=keepdims))
 
 
 # Not working with XArray yet, pending a fix for:
@@ -617,7 +614,7 @@ def cumulative_sum(
         if x_.dtype == "bool":
             x_ = akcast(x_, int)
     else:
-        x_ = Array(akcast(x._array, dtype))
+        x_ = akcast(x_, dtype)
 
     resp = generic_msg(
         cmd=f"cumSum<{x_.dtype},{x.ndim}>",
