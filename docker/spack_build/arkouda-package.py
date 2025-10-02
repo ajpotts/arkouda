94f30185d21aa85323a1cdf3d823847b6a6f03cc
ajpotts <ajpotts@users.noreply.github.com>
2025-10-02 12:26:18 -0400

    working

--- docker/spack_build/arkouda-package.py
+++ docker/spack_build/arkouda-package.py
@@ -3,14 +3,14 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 
-from spack.package import *
 from spack_repo.builtin.build_systems.makefile import MakefilePackage
+
+from spack.package import *
 
 
 class Arkouda(MakefilePackage):
     """Arkouda is a NumPy-like library for distributed data with a focus on
-    large-scale data science applications.
-    """
+    large-scale data science applications."""
 
     homepage = "https://github.com/Bears-R-Us/arkouda"
 
@@ -29,8 +29,12 @@ class Arkouda(MakefilePackage):
 
     version("main", branch="main")
 
-    version("2025.08.20", sha256="3e305930905397ff3a7a28a5d8cc2c9adca4194ca7f6ee51f749f427a2dea92c")
-    version("2025.07.03", sha256="eb888fac7b0eec6b4f3bfa0bfe14e5c8f15b449286e84c45ba95c44d8cd3917a")
+    version(
+        "2025.08.20", sha256="3e305930905397ff3a7a28a5d8cc2c9adca4194ca7f6ee51f749f427a2dea92c"
+    )
+    version(
+        "2025.07.03", sha256="eb888fac7b0eec6b4f3bfa0bfe14e5c8f15b449286e84c45ba95c44d8cd3917a"
+    )
     version(
         "2024.10.02",
         sha256="00671a89a08be57ff90a94052f69bfc6fe793f7b50cf9195dd7ee794d6d13f23",
@@ -53,7 +57,9 @@ class Arkouda(MakefilePackage):
         when="@2025.07.03:2025.08.20",
         type=("build", "link", "run", "test"),
     )
-    depends_on("chapel@2.1: +hdf5 +zmq", when="@:2025.01.13", type=("build", "link", "run", "test"))
+    depends_on(
+        "chapel@2.1: +hdf5 +zmq", when="@:2025.01.13", type=("build", "link", "run", "test")
+    )
 
     depends_on("cmake@3.13.4:", type="build")
     depends_on("python@3.9:", type=("build", "link", "run", "test"))
