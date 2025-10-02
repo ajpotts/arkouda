35bb1ea8e5a8505db887765e652ba336e534b46d
ajpotts <ajpotts@users.noreply.github.com>
2025-10-02 12:26:18 -0400

    working

--- docker/spack_build/py_arkouda-package.py
+++ docker/spack_build/py_arkouda-package.py
@@ -2,8 +2,9 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)s
 
-from spack.package import *
 from spack_repo.builtin.build_systems.python import PythonPackage
+
+from spack.package import *
 
 
 class PyArkouda(PythonPackage):
@@ -28,8 +29,12 @@ class PyArkouda(PythonPackage):
 
     version("main", branch="main")
 
-    version("2025.08.20", sha256="3e305930905397ff3a7a28a5d8cc2c9adca4194ca7f6ee51f749f427a2dea92c")
-    version("2025.07.03", sha256="eb888fac7b0eec6b4f3bfa0bfe14e5c8f15b449286e84c45ba95c44d8cd3917a")
+    version(
+        "2025.08.20", sha256="3e305930905397ff3a7a28a5d8cc2c9adca4194ca7f6ee51f749f427a2dea92c"
+    )
+    version(
+        "2025.07.03", sha256="eb888fac7b0eec6b4f3bfa0bfe14e5c8f15b449286e84c45ba95c44d8cd3917a"
+    )
     version(
         "2024.10.02",
         sha256="00671a89a08be57ff90a94052f69bfc6fe793f7b50cf9195dd7ee794d6d13f23",
